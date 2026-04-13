89a2c9ffaef3de4d0ec63679c9130243813f238c
lvmneo <157420045+lvmneo@users.noreply.github.com>
2026-04-13 12:55:22 +0300

    udp2

--- server/src/app.ts
+++ server/src/app.ts
@@ -20,6 +20,7 @@ app.use(cors({
     'http://localhost:4173',
     'http://127.0.0.1:5173',
     'http://localhost:5173',
+    'https://brand-6xqkgp1vj-lvmneos-projects.vercel.app/'
   ],
   credentials: true,
 }))
